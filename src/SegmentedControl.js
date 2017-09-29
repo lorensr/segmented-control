7b9532dd180462dcd19981ef3db988109c748c6c
Nguyễn Hoàng Huy <hoanghuy@dwarvesf.com>
2017-09-29 13:38:16 +0700

    support v16

--- src/SegmentedControl.js
+++ src/SegmentedControl.js
@@ -1,39 +1,39 @@
-import React, { PropTypes } from 'react'
-import _ from 'lodash'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+// import React, { PropTypes } from 'react'
 
-import './SegmentedControl.css'
+import _ from 'lodash';
 
-const SegmentedControl = React.createClass({
-  propTypes: {
+import './SegmentedControl.css';
+
+class SegmentedControl extends Component {
+  static propTypes = {
     name: PropTypes.string.isRequired,
     options: PropTypes.array.isRequired,
     style: PropTypes.object,
     setValue: PropTypes.func,
-  },
+  };
 
   componentWillMount() {
-    const defaultOption = _.find(this.props.options, {default: true})
-    this.setValue(defaultOption.value)
-  },
+    const defaultOption = _.find(this.props.options, { default: true });
+    this.setValue(defaultOption.value);
+  }
 
   setValue(val) {
-    this.props.setValue && this.props.setValue(val)
-  },
+    return this.props.setValue && this.props.setValue(val);
+  }
 
   render() {
-    const getId = option => this.props.name + option.value
+    const getId = option => this.props.name + option.value;
 
     const defaultStyle = {
-      width: '100%'
-    }
+      width: '100%',
+    };
 
-    const style = _.extend(defaultStyle, this.props.style)
+    const style = _.extend(defaultStyle, this.props.style);
 
     return (
-      <div
-        className="segmented-control"
-        style={style}
-        >
+      <div className="segmented-control" style={style}>
         {this.props.options.map(option => (
           <input
             key={option.value}
@@ -42,22 +42,21 @@ const SegmentedControl = React.createClass({
             id={getId(option)}
             defaultChecked={option.default}
             disabled={option.disabled}
-            />
-          ))
-        }
+          />
+        ))}
         {this.props.options.map(option => (
           <label
             key={option.value}
             onClick={() => this.setValue(option.value)}
             htmlFor={getId(option)}
             data-value={option.label}
-            >
+          >
             {option.label}
           </label>
         ))}
       </div>
     );
   }
-});
+}
 
 export default SegmentedControl;
